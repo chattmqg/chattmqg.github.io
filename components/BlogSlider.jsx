b5c28a3c47a15fc1ab610e3d5995f318b4515fab
Brent M. Spell <bspell1@gmail.com>
2016-11-13 15:11:41 -0500

    added images

--- components/BlogSlider.jsx
+++ components/BlogSlider.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import Fetch from 'react-fetch'
 
 const propTypes = {
   location: PropTypes.object.isRequired,
@@ -8,13 +9,36 @@ const propTypes = {
 function BlogSlider({ location }) {
   const post = location.query['post'];
 
+  function getFetchUri (post) {
+    console.log("post: " + post);
+    return "https://www.googleapis.com/drive/v3/files?q='" +
+           post +
+           "'+in+parents&key=AIzaSyDmj1s8QUQnoqwZCNTqdc15KPsjMAGaF2A";
+  }
+
   return (
     <div>
-      <div>
-        <div>blog post: {post}</div>
-      </div>
+      <div>blog post: {post}</div>
+       <Fetch url={getFetchUri(post)}>
+        <TestComponent/>
+       </Fetch>
     </div>
   );
+}
+
+class TestComponent extends React.Component {
+  render() {
+    const images = [];
+    if (this.props.files)
+      for (var file of this.props.files) {
+        var uri = "https://drive.google.com/uc?export=download&id=" +
+                  file.id;
+        images.push(
+          <img key={file.id} src={uri} width="50px" height="50px"></img>
+        );
+      }
+    return (<div>{images}</div>);
+  }
 }
 
 BlogSlider.propTypes = propTypes;
