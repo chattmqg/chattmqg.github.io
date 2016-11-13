6639011e30025f5154ad621e1cb7e07805473833
Brent M. Spell <bspell1@gmail.com>
2016-11-13 16:07:45 -0500

    add image slider

--- components/BlogSlider.jsx
+++ components/BlogSlider.jsx
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import Fetch from 'react-fetch'
+import Slider from 'react-slick';
+import styles from './slider.css';
 
 const propTypes = {
   location: PropTypes.object.isRequired,
@@ -10,7 +12,6 @@ function BlogSlider({ location }) {
   const post = location.query['post'];
 
   function getFetchUri (post) {
-    console.log("post: " + post);
     return "https://www.googleapis.com/drive/v3/files?q='" +
            post +
            "'+in+parents&key=AIzaSyDmj1s8QUQnoqwZCNTqdc15KPsjMAGaF2A";
@@ -18,29 +19,49 @@ function BlogSlider({ location }) {
 
   return (
     <div>
-      <div>blog post: {post}</div>
-       <Fetch url={getFetchUri(post)}>
-        <TestComponent/>
-       </Fetch>
+      <Fetch url={getFetchUri(post)}>
+        <SliderWrapper/>
+      </Fetch>
     </div>
   );
 }
 
-class TestComponent extends React.Component {
+BlogSlider.propTypes = propTypes;
+
+class SliderWrapper extends React.Component {
   render() {
-    const images = [];
-    if (this.props.files)
-      for (var file of this.props.files) {
-        var uri = "https://drive.google.com/uc?export=download&id=" +
-                  file.id;
-        images.push(
-          <img key={file.id} src={uri} width="50px" height="50px"></img>
-        );
-      }
-    return (<div>{images}</div>);
+    const settings = {
+      dots:           true,
+      infinite:       true,
+      speed:          500,
+      slidesToShow:   1,
+      slidesToScroll: 1,
+      lazyLoad:       true
+    };
+    console.log(styles.image);
+    const images = this.props.files.map(function(file) {
+      var uri = 'https://drive.google.com/uc?export=download&id=' +
+                file.id;
+      return (
+        <div key={file.id}>
+          <img src={uri} className={styles.image}/>
+        </div>
+      );
+    });
+    if (images.length === 0)
+      return <div/>
+    else
+      return (
+        <div>
+          <Slider {...settings}>
+            {images}
+          </Slider>
+        </div>
+      );
   }
 }
-
-BlogSlider.propTypes = propTypes;
+SliderWrapper.defaultProps = {
+  files: []
+};
 
 export default BlogSlider;
